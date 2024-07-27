440efa824a4301903b512168e07478e343a7402f
wanglinglei <18627024279@163.com>
2024-07-27 14:02:25 +0800

    eslint check

---
+++ commitlint.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      "2",
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "style",
+        "test",
+        "refactor",
+      ],
+    ],
+  },
+};
